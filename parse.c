5607e799f3d0f361530484712d75dc0ce4e3b0c6
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-21 18:28:25 +0200

    used strok to parse args

---
+++ parse.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+int parse_cmd(char *cmd, char **args)
+{
+    int arg_count = 0;
+    args[arg_count++] = strtok(cmd, " \n");
+
+    while (arg_count < MAX_ARGS && args[arg_count-1] != NULL)
+    {
+        args[arg_count++] = strtok(NULL, " \n");
+    }
+
+    return arg_count;
+}
